4fffc0d1bdf3dfd6de396d9d539af626e8715f3d
Sanjaya Harshana <sanjayaharasana@gmail.com>
2025-07-28 01:47:25 +0530

    Landing Controlle rUpdates

--- Modules/Landing/app/Http/Controllers/LandingController.php
+++ Modules/Landing/app/Http/Controllers/LandingController.php
@@ -12,7 +12,7 @@ class LandingController extends Controller
      */
     public function index()
     {
-        return view('landing::index');
+        return view('landing::index-new');
     }
 
     /**
